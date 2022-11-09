91bdb4a6d45e2bdcdbedd1e2b1ab6e4c949bb9e8
ZohalZShukoor <zohal.shukoor@outlook.com>
2022-11-08 18:53:58 -0800

    this is on practice branch

--- src/test/Branching.java
+++ src/test/Branching.java
@@ -5,6 +5,14 @@ public class Branching {
 	public static void main(String[] args) {
 		System.out.println("this is a branching test");
 
+		public void iCanLearnGit() {
+			System.out.println("I learn this or i will practice the rest of the night");
+		}
+		
+		public void thisIsFunWhenItWorks () {
+			System.out.println("ok");
+			
+		}
 	}
 
 }
